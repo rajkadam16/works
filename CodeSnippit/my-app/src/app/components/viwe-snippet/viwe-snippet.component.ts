730faaf2f267c95bd2f2cc21376e108dd5d298d5
rajkadam16 <152425457+rajkadam16@users.noreply.github.com>
2024-05-20 22:04:32 +0530

    code snippet

--- CodeSnippit/my-app/src/app/components/viwe-snippet/viwe-snippet.component.ts
+++ CodeSnippit/my-app/src/app/components/viwe-snippet/viwe-snippet.component.ts
@@ -12,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ViweSnippetComponent {
   codeSnippet = {
     title:"",
-    code:""
+    code:''
   }
   constructor(private route: ActivatedRoute,private dbService: DbService) { }
 
